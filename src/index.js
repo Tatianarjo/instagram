5956dd3a18c804ce2e873ff8048c65b1ffd93fc4
Tatiana Johnson <Tatianarjo@gmail.com>
2021-12-06 23:05:09 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ 
+    <App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// client side rendered app: react (cra)
+  // > database which is Firebase
+    // react-loading-skeleton
+    // tailwind
+
+    //  Folder Structure
+       // src
+        // components, 
+        // constants, 
+        //context, 
+        //helpers,
+        //hooks,
+        //pages, 
+        //lib (firebase is ging to live here)
+        // services (firebase functions here)
+        // styles (tailwind's folder(app/tailwind))
