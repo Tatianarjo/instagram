cbb66bda6c53c939ba8c8f0626bd8d0bc9c171c4
Tatiana Johnson <Tatianarjo@gmail.com>
2022-07-28 08:36:30 -0500

    added css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 
 function App() {
   return (
-    <p>Hello TotGram</p>
+    <div className="App">
+    <h1>Hello TotGram</h1>
+
+    <div className="app__header">
+
+    </div>
+    {/* Header */}
+
+    {/* Posts */}
+    {/* Posts */}
+    </div>
   );
 }
 
 export default App;
+
+
+// LINK: https://www.youtube.com/watch?v=f7T48W0cwXM
+
+//TIME: 7-28 at 14:48
